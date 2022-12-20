b0d5681620dd4359c546ef8dea1aa381815f46e5
touria <touriamt69@gmail.com>
2022-12-20 09:10:52 -0500

    got the timer to work, time to sleep

---
+++ game/app.js
@@ -0,0 +1,53 @@
+
+const secondsStart = 30;
+let time = secondsStart * 60;
+const display = document.querySelector(".time-display");
+
+const flower = document.querySelector(".flower");
+const gameContainer = document.querySelector("#container");
+
+    let seconds= 30;
+    let interval = null;
+    
+// setInterval(() => {
+//     seconds--;
+    
+//     if(seconds === 30){
+//      display.innerText = `${sec}`;
+//     display.innerText = seconds;   
+//     }
+    
+    
+// }, 3000);
+
+gameContainer.addEventListener('click', function(){
+
+    setInterval(function(){
+        seconds -= 1;
+        display.innerText = seconds;
+        console.log("hi");
+    }, 1000);
+    
+    }
+    
+)
+    
+    
+//     seconds--;
+//     let sec = seconds % 60;
+//     display.innerText = `${sec}`;
+//     if(seconds == 15){
+//      display.innerText = `${sec}`;
+//     display.innerText = seconds;   
+//     }
+//     if(display.innerText <= 0){
+//         display.innerText= ('winner');
+//     }
+//     }
+
+    
+    
+
+// );
+
+
