88b3cd403a50eeff269a9c74e8d9b6d8993c92a2
touria <touriamt69@gmail.com>
2022-12-20 20:51:47 -0500

    last step

--- game/app.js
+++ game/app.js
@@ -5,28 +5,74 @@ const display = document.querySelector(".time-display");
 
 const flower = document.querySelector(".flower");
 const gameContainer = document.querySelector("#container");
-
+const bee = document.querySelector(".bee");
     let seconds= 30;
     let interval = null;
-    
-// setInterval(() => {
-//     seconds--;
-    
-//     if(seconds === 30){
-//      display.innerText = `${sec}`;
-//     display.innerText = seconds;   
-//     }
-    
-    
-// }, 3000);
+    let win = ('Flower is Pollinated, you win!')
 
+
+
+// eventlisteners //
 gameContainer.addEventListener('click', function(){
+bee.addEventListener('dragstart', function(){
 
+        console.log('start');
+        setTimeout(() => this.className = 'invisble', 0);}
+    )
+
+    bee.addEventListener('dragend', function(){
+        console.log('end');
+        this.className = 'bee';
+    })
+
+    flower.addEventListener('dragover', function(e){
+        e.preventDefault();
+        console.log('bee is pollinating me')
+        if(display.innerText <= 0){
+            display.innerText =('Flower is Pollinated, you win!')}
+        
+    })
+
+    flower.addEventListener('drop', function(){
+        console.log('im giving the bee some nectar')
+        this.className = 'flower';
+        this.append(bee);
+        if(display.innerText <=30){
+        
+        display.innerText =('Flower is Pollinated, you win!')}
+        
+        if (display.innerText = 'Flower is Pollinated, you win!') {
+            
+        }
+        if(display.innertext == 0){
+            display.innerText= 'game over';
+        }
+    } )
+
+
+    
+    if(bee){}
     setInterval(function(){
         seconds -- ;
         display.innerText = seconds;
-        if(display.innerText <=0){
+        
+        if(display.innerText <= 0){
             display.innerText =('game over');
+        
+        if(display.innerText < 30){
+            display.innerText =('Flower is Pollinated, you win!')
+        }
+       
+        if (display.innerText == 'Flower is Pollinated, you win!') {
+            
+        }
+        // if(display.innerText = 'Flower is Pollinated, you win!'){
+
+        //     // if(display.innerText = ('Flower is Pollinated, you win!')){
+            
+        //     display.innerText = 'Flower is Pollinated, you win!'
+            
+        // }
         }
     }, 1000);
     
@@ -35,21 +81,6 @@ gameContainer.addEventListener('click', function(){
 )
     
     
-//     seconds--;
-//     let sec = seconds % 60;
-//     display.innerText = `${sec}`;
-//     if(seconds == 15){
-//      display.innerText = `${sec}`;
-//     display.innerText = seconds;   
-//     }
-//     if(display.innerText <= 0){
-//         display.innerText= ('winner');
-//     }
-//     }
-
-    
-    
-
-// );
+    //https://www.youtube.com/watch?v=C22hQKE_32c&ab_channel=TraversyMedia dragend +dragfill help //
 
 
